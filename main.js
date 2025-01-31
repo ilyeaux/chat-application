91d6d2bd754ddef0cb90e06791e73f4e770423c3
Vitalii Braun <vitossvx@gmail.com>
2025-02-01 00:26:42 +0300

    Revert "update chat app"
    This reverts commit bcc445c712ded3e807b3acbd60b15ee3c6c4e7e5.

--- main.js
+++ main.js
@@ -19,32 +19,13 @@ app.use(express.static("public"));
 io.on("connection", (socket) => {
   console.log("New client connected");
 
-  // Listen for the 'set username' event
-  socket.on("set username", (username) => {
-    socket.username = username; // Store the username in the socket object
-    console.log(`${username} has joined the chat`);
-    io.emit("chat message", {
-      username: "System",
-      message: `${username} has joined the chat`,
-    });
-  });
-
-  // Listen for the 'chat message' event
   socket.on("chat message", (msg) => {
     console.log("Received message:", msg); // Log received messages
-    io.emit("chat message", { username: socket.username, message: msg }); // Broadcast the message with the username
+    io.emit("chat message", msg); // Broadcast the message to all clients
   });
 
   socket.on("disconnect", () => {
-    if (socket.username) {
-      console.log(`${socket.username} has left the chat`);
-      io.emit("chat message", {
-        username: "System",
-        message: `${socket.username} has left the chat`,
-      });
-    } else {
-      console.log("A client has disconnected");
-    }
+    console.log("Client disconnected");
   });
 });
 
