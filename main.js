923713014febc3de6b79ac0e1975da8d0dcbbabd
ilyeaux <ilyeaux@gmail.com>
2025-01-31 22:21:56 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('New client connected');
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log('Server is running on port ${PORT}');
+});
